198ec10e7d0905d55cc56d0109fd10e8aac8668a
Patrick Malone <ucfebra@gmail.com>
2023-04-23 22:23:21 -0400

    Added color and text python

---
+++ RGBColor.py
@@ -0,0 +1,75 @@
+import random
+
+class RGBColor:
+    def __init__(self, text_color=(0,0,0), bg_color=(255,255,255), outline_color=(0,0,0)):
+        self.text_color = text_color
+        self.bg_color = bg_color
+        self.outline_color = outline_color
+    
+    def set_text_color(self, red, green, blue):
+        self.text_color = (red, green, blue)
+    
+    def set_bg_color(self, red, green, blue):
+        self.bg_color = (red, green, blue)
+    
+    def set_outline_color(self, red, green, blue):
+        self.outline_color = (red, green, blue)
+    
+    def randomize(self):
+        self.text_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.bg_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.outline_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        
+    def set_predefined_color(self, color):
+        if color == "red":
+            self.text_color = (255, 0, 0)
+        elif color == "green":
+            self.text_color = (0, 255, 0)
+        elif color == "blue":
+            self.text_color = (0, 0, 255)
+        elif color == "yellow":
+            self.text_color = (255, 255, 0)
+        elif color == "purple":
+            self.text_color = (255, 0, 255)
+        elif color == "turquoise":
+            self.text_color = (0, 255, 255)
+    
+    def get_text_color(self):
+        return self.text_color
+    
+    def get_bg_color(self):
+        return self.bg_color
+    
+    def get_outline_color(self):
+        return self.outline_color
+    
+    def is_legible(self):
+        # Calculate relative luminance of text color and background color
+        # For more information: https://www.w3.org/TR/WCAG20/#relativeluminancedef
+        def calculate_luminance(color):
+            r, g, b = color
+            r_linear = r / 255.0 if r <= 10 else ((r / 255.0) ** 2.2)
+            g_linear = g / 255.0 if g <= 10 else ((g / 255.0) ** 2.2)
+            b_linear = b / 255.0 if b <= 10 else ((b / 255.0) ** 2.2)
+            return 0.2126 * r_linear + 0.7152 * g_linear + 0.0722 * b_linear
+        
+        # Compare relative luminance of text color and background color
+        text_luminance = calculate_luminance(self.text_color)
+        bg_luminance = calculate_luminance(self.bg_color)
+        if text_luminance > bg_luminance:
+            contrast_ratio = (text_luminance + 0.05) / (bg_luminance + 0.05)
+        else:
+            contrast_ratio = (bg_luminance + 0.05) / (text_luminance + 0.05)
+        
+        # Check if contrast ratio is sufficient for legibility
+        return contrast_ratio
+
+            
+def main() ->int:
+    colors = RGBColor ()    
+    
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ text.py
@@ -0,0 +1,57 @@
+#python2
+import time
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/..'))
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from rgbmatrix import graphics
+
+from rgb_color import RGBColor
+
+# Set up RGB matrix options
+options = RGBMatrixOptions()
+options.rows = 32
+options.cols = 64
+options.chain_length = 2
+options.parallel = 1
+options.hardware_mapping = 'adafruit-hat'
+matrix = RGBMatrix(options=options)
+
+# Set up RGBColor object for text, background, and outline colors
+color = RGBColor((255, 255, 255), (0, 0, 0), (0, 0, 0))
+
+# Create graphics object and set font
+font = graphics.Font()
+font.LoadFont("../../../fonts/5x8.bdf")
+text_graphics = graphics.Drawer(matrix)
+
+# Set up two lines of text to display
+line1 = "Hello world!"
+line2 = "This is a test."
+
+# Set text positions and draw text
+x1 = 1
+y1 = 9
+x2 = 1
+y2 = 19
+text_graphics.SetFont(font)
+text_graphics.SetTextColor(*color.get_text_color())
+text_graphics.SetBackgroundColor(*color.get_bg_color())
+text_graphics.SetTextWrap(False)
+
+# Validate legibility of text and background colors
+if not color.is_legible():
+    sys.exit("Text color and background color do not have enough contrast to be legible.")
+
+# Draw first line of text with outline
+text_graphics.DrawText(x1, y1, color.get_outline_color(), line1)
+text_graphics.DrawText(x1, y1, color.get_text_color(), line1)
+
+# Draw second line of text with outline
+text_graphics.DrawText(x2, y2, color.get_outline_color(), line2)
+text_graphics.DrawText(x2, y2, color.get_text_color(), line2)
+
+# Wait for 5 seconds before clearing display
+time.sleep(5)
+matrix.Clear()
